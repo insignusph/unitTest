4ec3c7655ca0b648e4b9679dad0f253f71cf1ffb
Paulo Silva <p.silva@n006251.conductor.tecnologia>
2021-04-20 18:05:11 -0300

    first commit

---
+++ spec/example/exampleSpec.js
@@ -0,0 +1,9 @@
+describe('Meu primeiro Teste', function() {
+    it('Deve demonstar o uso do Jasmine', function() {
+        expect(true).toEqual(true)
+    });
+
+    it('Deve demonstar o uso do karma em modo escuta', function() {
+        expect(true).toEqual(true)        
+    });
+})
